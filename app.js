3e910e054ee903b92827200b4d45da971b795038
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-13 14:24:27 +0600

    i am create heaireorder function in app.js file

--- app.js
+++ app.js
@@ -10,4 +10,29 @@
  * - closure
  * - Function Context
  * - Hostaing 
- *  */ 
+ *  */
+
+function genarateTwoRandomNumbers(Max, callback) {
+    const random1 = Math.floor(Math.random() * Max)
+    const random2 = Math.floor(Math.random() * Max)
+    const result = callback(random1, random2);
+    return result
+}
+
+const sum = genarateTwoRandomNumbers(100, (num1, num2) => {
+    return num1 + num2;
+})
+
+const multiply = genarateTwoRandomNumbers(100, (random1, random2) => random1 * random2);
+
+console.log(sum, 'sum');
+console.log(multiply, 'multiply');
+
+// function randomSum(Max) {
+//     const { random1, random2 } = genarateTwoRandomNumbers(20)
+//     return random1 + random2;
+// }
+// function randomSub(Max) {
+//     const { random1, random2 } = genarateTwoRandomNumbers(20)
+//     return random1 + random2;
+// }
