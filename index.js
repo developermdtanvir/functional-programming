957a24b3e9491f3d17ba08acc7c37fb80abe2941
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-07-13 08:10:50 +0600

    add haiger order function

---
+++ index.js
@@ -0,0 +1,34 @@
+// this is function statement
+function addNumber(number) {
+
+}
+
+// this is anonymous function
+const myFunc = function () {
+
+}
+
+// this is arrow function 
+const addString = () => {
+
+}
+
+
+function filterFunction(arr, callback) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i]) ? filteredArr.push(arr[i]) : null;
+
+    }
+    return filteredArr;
+}
+
+function isOdd(x) {
+    return x % 2 !== 0;
+}
+
+const data = filterFunction([20, 30, 50, 60, 70, 80, 90, 31, 32, 33, 34, 35,], isOdd)
+
+console.log(data);
+
+
